bd5bf2143cf5aaea465ed016cab53b4412551df5
Mehmed Suleiman <mehmedsuleiman141@gmail.com>
2026-01-14 17:08:13 +0200

    exercise 6

---
+++ _03_exercise_stacks_queues_tuples_and_sets/1_numbers.py
@@ -0,0 +1,13 @@
+#First, you will be given two sequences of integer values on different lines.
+# The values of the sequences are separated by a single space.
+#Keep in mind that each sequence should contain only unique values.
+#Next, you will receive a number - N. On the following N lines, you will receive one of the following commands:
+#•	"Add First {numbers, separated by a space}" - add the given numbers at the end of the first sequence of numbers.
+#•	"Add Second {numbers, separated by a space}" - add the given numbers at the end of the second sequence of numbers.
+#•	"Remove First {numbers, separated by a space}" - remove only the numbers contained in the first sequence.
+#•	"Remove Second {numbers, separated by a space}" - remove only the numbers contained in the second sequence.
+#•	"Check Subset" - check if any of the given sequences are a subset of the other.
+# If it is, print "True". Otherwise, print "False".
+#In the end, print the final sequences, separated by a comma and a space ", ".
+# The values in each sequence should be sorted in ascending order.
+
