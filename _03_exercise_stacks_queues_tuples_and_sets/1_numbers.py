ea3f778ac268bab4e310c497cc798b340b214afd
Mehmed Suleiman <mehmedsuleiman141@gmail.com>
2026-01-16 16:29:10 +0200

    exercise 1

--- _03_exercise_stacks_queues_tuples_and_sets/1_numbers.py
+++ _03_exercise_stacks_queues_tuples_and_sets/1_numbers.py
@@ -11,3 +11,36 @@
 #In the end, print the final sequences, separated by a comma and a space ", ".
 # The values in each sequence should be sorted in ascending order.
 
+
+
+first_set = set(int(x) for x in input().split())
+second_set = set(int(x) for x in input().split())
+
+n = int(input())
+
+for _ in range(n):
+    command = input().split()
+    if command[0] == "Add":
+        numbers = set(int(x) for x in command[2:])
+        if command[1] == "First":
+            first_set.update(numbers)
+        elif command[1] == "Second":
+            second_set.update(numbers)
+    elif command[0] == "Remove":
+        numbers = set(int(x) for x in command[2:])
+        if command[1] == "First":
+            first_set.difference_update(numbers)
+        elif command[1] == "Second":
+            second_set.difference_update(numbers)
+    elif command[0] == "Check" and command[1] == "Subset":
+        if first_set.issubset(second_set):
+            print("True")
+        elif second_set.issubset(first_set):
+            print("True")
+        else:
+            print("False")
+
+
+
+print(*sorted(first_set), sep=", ")
+print(*sorted(second_set), sep=", ")
