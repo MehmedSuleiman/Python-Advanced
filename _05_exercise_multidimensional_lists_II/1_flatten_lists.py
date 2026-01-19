3941d4fd31db27589a4b6f033c852c1ddd0f361d
Mehmed Suleiman <mehmedsuleiman141@gmail.com>
2026-01-19 16:41:36 +0200

    exercise 1

---
+++ _05_exercise_multidimensional_lists_II/1_flatten_lists.py
@@ -0,0 +1,11 @@
+#Write a program to flatten several lists of numbers received in the following format:
+#	String with numbers or empty strings separated by "|"
+#	Values are separated by spaces (" ", one or several)
+#	Order the output list from the last to the first matrix sub-lists and their values from left to right as shown below
+
+numbers = input().split("|")
+
+matrix = [row.split() for row in numbers]
+
+for row in reversed(matrix):
+    print(*row, end=" ")
