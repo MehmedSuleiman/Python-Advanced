4a397d00ff3a41df45ff382ba45fbbc6b5da9234
Mehmed Suleiman <mehmedsuleiman141@gmail.com>
2026-01-22 14:15:55 +0200

    exercise 5

---
+++ _06_fxercise_functions_advanced/5_concatenate.py
@@ -0,0 +1,18 @@
+#Write a concatenate() function that receives some strings as arguments and some named
+# arguments (the key will be a string, and the value will be another string).
+#First, you should concatenate all arguments successively. Next, take each key successively, and if
+# it is present in the resulting string, change all matching parts with the key's value.
+# In the end, return the final string.
+# See the examples for more clarification.
+#Submit only your function in the judge system.
+from operator import index
+
+
+def concatenate(*args, **kwargs):
+    string = "".join(args)
+    for key, value in kwargs.items():
+        if key in string:
+            string = string.replace(key, value)
+    return string
+
+print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
